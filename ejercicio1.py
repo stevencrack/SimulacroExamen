95083644c2392750ded9856a7820b1930faa2644
stevencrack-simulacroexamen-wwfiplfs <stevencrack-simulacroexamen-wwfiplfs@local>
2018-05-24 21:50:54 +0000

    ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,7 @@
 #Escriba el codigo necesario para que al ejecutar
 #python ejercicio1.py se impriman los enteros del 10 al 1 en cuenta regresiva.
+
+import numpy as np
+
+for i in range(11):
+    print (10-i)
